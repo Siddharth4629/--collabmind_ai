7e70e1bd28a60eef0428819ea13d632f0769fc27
Siddharth4629 <siddharthsarda742@gmail.com>
2026-05-30 11:48:03 +0530

    Restrict CORS to production frontend URL

--- server/server.js
+++ server/server.js
@@ -54,11 +54,12 @@ async function startServer() {
 
   app.use(express.json());
 
-  app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true
-  }));
+  app.use(
+    cors({
+      origin: 'https://collabmind-frontend.onrender.com',
+      credentials: true
+    })
+  );
 
   app.use(helmet({
     contentSecurityPolicy: false,
